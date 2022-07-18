3a32ebb4b18692cb1b539c1471480a7d4540325f
d-tsu <dtsu3j@gmail.com>
2022-07-18 22:07:39 +0900

    fix

--- restart.sh
+++ restart.sh
@@ -9,7 +9,7 @@ systemctl restart nginx
 
 info "===== logrotate nginx =========="
 ACCESS_LOG="/var/log/nginx/access.log"
-if [[ -s "${ACCESS_LOG}" ]]
+if [[ -s "${ACCESS_LOG}" ]]; then
     cp -ap ${ACCESS_LOG} ${ACCESS_LOG}.$(date +%Y%m%d-%H%M%S)
     rm ${ACCESS_LOG}
     nginx -s reopen
