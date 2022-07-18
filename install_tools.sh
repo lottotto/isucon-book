92c96fc0c622e4b6c550b739dada249988393f38
root <root@app>
2022-07-18 21:47:35 +0900

    修正

--- install_tools.sh
+++ install_tools.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 source util.sh
 
-wget https://github.com/tkuchiki/alp/releases/download/v1.0.10/alp_linux_amd64.zip -O /tmp/alp.zip
-install /tmp/alp.zip /usr/local/bin/alp
+wget https://github.com/tkuchiki/alp/releases/download/v1.0.10/alp_linux_amd64.tar.gz -O /tmp/alp.tar.gz
+tar xzf /tmp/alp.tar.gz -C /tmp
+install /tmp/alp /usr/local/bin/alp
+
+info "===== verify installation ====="
+alp --version
