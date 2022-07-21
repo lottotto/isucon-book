34c5c27273d32305840e09f39c246c2bd6dd8e9e
d-tsu <dtsu3j@gmail.com>
2022-07-22 03:21:05 +0900

    pt, netdata

--- install_tools.sh
+++ install_tools.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 source util.sh
 
+info "===== install alp ====="
 wget https://github.com/tkuchiki/alp/releases/download/v1.0.10/alp_linux_amd64.tar.gz -O /tmp/alp.tar.gz
 tar xzf /tmp/alp.tar.gz -C /tmp
 install /tmp/alp /usr/local/bin/alp
 
 info "===== verify installation ====="
 alp --version
+
+info "===== install percona-toolkit ====="
+apt update
+apt install percona-toolkit
+
+info "===== verify installation ====="
+pt-query-digest --version
+
+info "===== install netdata ====="
+# https://learn.netdata.cloud/docs/agent/packaging/installer/methods/kickstart
+wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --no-updates --stable-channel --disable-telemetry
+
+info "===== verify installation ====="
+curl localhost:19999
